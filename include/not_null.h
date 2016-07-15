36c1b2d0667cd3a39632cb7e0cd8bfcd915d4725
Peter Ballok <peter.ballok@logmein.com>
2016-07-15 17:07:51 +0200

    Add Conversion from compatible types

--- include/not_null.h
+++ include/not_null.h
@@ -1,5 +1,25 @@
+#include <type_traits>
+
 template <typename P>
 class not_null {
+
+	static_assert(std::is_pointer<P>::value, "This template can only be instantiated with a pointer type");
+
 	public:
-		not_null(P) {}
+		not_null(P ptr) : ptr_(ptr) { }
+
+		template <typename FromType>
+		not_null(FromType f) {
+			static_assert(std::is_convertible<FromType, P>::value, "Cannot convert type to required pointer type");
+			ptr_ = f;
+		}
+
+		not_null(int) = delete;
+		not_null(std::nullptr_t) = delete;
+
+		P get() const { return ptr_; }
+		decltype(*P()) operator *() const { return *ptr_; }
+
+	private:
+		P ptr_;
 };
--- tests/not_null.cpp
+++ tests/not_null.cpp
@@ -1,7 +1,51 @@
 #include <gtest/gtest.h>
 #include <not_null.h>
 
-TEST(not_null, RawPointer) {
-	int  i = 0;
-	not_null<int*> p(&i);
+class A {
+public:
+	explicit A(int i) : i_(i) {}
+	void set(int i) { i_ = i; }
+	int get() const { return i_; }
+
+private:
+	int i_ = 0;
+};
+
+class B : public A {
+public:
+	explicit B(int i) : A(i) {}
+};
+
+TEST(not_null, RawPointerGet) {
+	A  a(10);
+	not_null<A*> p(&a);
+
+	EXPECT_EQ(10, a.get());
+
+	p.get()->set(42);
+
+	EXPECT_EQ(42, a.get());
+}
+
+TEST(not_null, RawPointerDereference) {
+	A  a(10);
+	not_null<A*> p(&a);
+
+	EXPECT_EQ(10, a.get());
+
+	(*p).set(42);
+
+	EXPECT_EQ(42, a.get());
+}
+
+TEST(not_null, RawPointerCompatible) {
+	B b(10);
+
+	not_null<A*> p(&b);
+
+	EXPECT_EQ(10, (*p).get());
+
+	(*p).set(42);
+
+	EXPECT_EQ(42, (*p).get());
 }
